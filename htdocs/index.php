9fa7a2c02e0284faae9130824d0506868534d9f7
StevePike <stevepike26@gmail.com>
2016-02-15 15:34:30 +0000

    Start

---
+++ htdocs/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Steven Pike Designs</title>
+	
+	<!-- CSS -->
+	
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../css/custom.css" type="text/css" title="myStyleSheet">
+	
+	
+	 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+  </head>
+  <body>
+	<!-- Bootstrap Jquery must be before Javascript -->
+	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+    
+	
+	<!-- Navigation bar-->
+		<!-- <nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+				<a href="" class="navbar-brand">Steven Pike Designs</a>
+				</div>
+				<div class="collapse navbar-collapse navbar-right" id="myNavbar">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">About Me</a></li> 
+						<li><a href="#">Contact</a></li> 
+					</ul>
+				</div>
+				
+			</div>
+		</nav> -->
+		
+		<!-- Main front page properties -->
+
+		<div class="main">
+		<div class="container">
+		<div class="row">
+
+		
+		</div>
+		</div>
+		</div>
+
+
+		
+		
+		
+
+	
+	
+  </body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,5 @@
+  <?php
+	// Redirect people to the serving directory, htdocs.
+	  header("Location: htdocs/");
+	// Looking for the REAL index.php? Try the serving directory, htdocs/index.php
+  ?>
